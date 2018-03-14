d3e3ff8d6593857d469ef890c288316cfa1b9261
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-14 05:08:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,6 @@ def speak_to_grandma(message)
   elsif message == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
-    return "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   end
 end
